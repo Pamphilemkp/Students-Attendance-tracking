7c7de48c4af187865b004bf5d409ef2d7758551d
pamphilemkp <pamphilemkp@gmail.com>
2023-05-25 16:35:56 +0300

    add bootstrap yarn and its dependancies

--- app/controllers/static_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaticPagesController < ApplicationController
-  def welcome_page
-  end
-
-  def privacy_policy
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/welcome_page'
-  get 'static_pages/privacy_policy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   root "static_pages#welcome_page"
+  # root "articles#index"
 end
