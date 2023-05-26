fed72c6b9b710e17da76be3b12ae577becd1764d
pamphilemkp <pamphilemkp@gmail.com>
2023-05-26 20:08:48 +0300

    add user authentication with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+    protected
+
+    def after_sign_in_path_for(resource)
+        privacy_policy_path
+    end
+  
+    def after_sign_out_path_for(resource_or_scope)
+        root_path
+      end
 end
--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,4 +1,5 @@
 class LandingPagesController < ApplicationController
+  before_action :authenticate_user!, only: [:privacy_policy]
   def welcome_page
   end
 
