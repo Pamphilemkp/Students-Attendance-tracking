1e43d85e3248d50afbe4c3533c79f900056a4c15
pamphilemkp <pamphilemkp@gmail.com>
2023-05-27 17:24:38 +0300

    add confirmable to devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
 
+    before_action :update_allowed_parameters, if: :devise_controller?
+  
     protected
+  
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.permit(:account_update) do |u|
+        u.permit(:name, :email, :password, :password_confirmation, :current_password)
+      end
+    end
 
     def after_sign_in_path_for(resource)
         privacy_policy_path
@@ -8,5 +18,5 @@ class ApplicationController < ActionController::Base
   
     def after_sign_out_path_for(resource_or_scope)
         root_path
-      end
+    end
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,11 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "pamphilemkp@gmail.com"
   layout "mailer"
+
+  def confirmation_instructions(record, token, opts={})
+  headers["Custom-header"] = "Bar"
+  opts[:subject] = "Confirmation instructions"
+  opts[:from] = "pamphilemkp@gmail.com"
+  super
+end
 end
