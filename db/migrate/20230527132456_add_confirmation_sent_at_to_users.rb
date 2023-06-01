1a6a283f09dce4c482c1360073b66d7778e690a6
pamphilemkp <pamphilemkp@gmail.com>
2023-06-01 16:43:07 +0300

    fixing duplacte heroku column

--- db/migrate/20230527132456_add_confirmation_sent_at_to_users.rb
+++ db/migrate/20230527132456_add_confirmation_sent_at_to_users.rb
@@ -1,5 +1,5 @@
 class AddConfirmationSentAtToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :confirmation_sent_at, :datetime
+    #add_column :users, :confirmation_sent_at, :datetime
   end
 end
